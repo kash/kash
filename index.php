fca6128c2a5f702a420024f03a8ad7a08c37ab45
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-04-25 19:07:05 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <link rel="stylesheet" href="/css/reset.css" type="text/css">
+    <link rel="stylesheet" href="/css/index.css" type="text/css">
+
+    <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+    <meta charset="UTF-8"/>
+    <meta name="description" content="Developer."/>
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@kashgoudarzi"/>
+    <meta name="twitter:title" content="Kash Goudarzi"/>
+    <meta name="twitter:description" content="Developer."/>
+    <meta name="twitter:creator" content="@kashgoudarzi"/>
+    <meta property="og:title" content="Kash Goudarzi">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://kash.cm/">
+    <meta property="og:description" content="Developer.">
+    <meta property="og:site_name" content="Kash Goudarzi">
+    <meta itemprop="url" content="http://kash.cm/">
+    <meta itemprop="name" content="Kash Goudarzi">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:100,300,500,900,700,500' rel='stylesheet' type='text/css'>
+    <title>Kash Goudarzi</title>
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="info">
+            <div class="section">
+                <h1 style="color: #46DCAD">Hey.</h1>
+                <h1>I'm Kash Goudarzi.</h1>
+            </div>
+            <div class="section websites">
+                <h3>Websites:</h3>
+                <p><a target="_blank" href="http://puretasks.com">puretasks.com</a></p>
+                <p><a target="_blank" href="http://blueout.co">blueout.co</a></p>
+                <p><a target="_blank" href="http://wesaturate.com">wesaturate.com</a></p>
+            </div>
+            <div class="section">
+                <h3>Photography:</h3>
+                <p><a target="_blank" href="http://unsplash.com/kash">unsplash.com/kash</a></p>
+                <p><a target="_blank" href="http://wesaturate.com/kash">wesaturate.com/kash</a></p>
+                <p><a target="_blank" href="http://instagram.com/kashgoudarzi">instagram.com/kashgoudarzi</a></p>
+            </div>
+            <div class="section">
+                <p><a style="color: #444" href="mailto:me@kash.cm">me@kash.cm</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+<script type="text/javascript" src="/js/library.js"></script>
+
+</html>
+
+

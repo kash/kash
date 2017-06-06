5e58844320469bbb00e101de8bf18c94112fcdef
Kash Goudarzi <kashgoudarzi@gmail.com>
2017-06-06 16:06:53 -0700

    recommiting everything

---
+++ server/app.js
@@ -0,0 +1,39 @@
+/**
+ * Setting up express server and requirements for server to run. Server can be started by
+ * doing 'npm start node'
+ */
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Settings global variables
+global.app = app;
+
+require('./upto.js');
+
+/**
+ * Serving client directory so that server has access to all JavaScript and CSS files.
+ *
+ * Directing all routes to index.html, hence the '*'
+ */
+app.use('/client', express.static(__dirname + '/../client'));
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, '../', 'index.html'));
+});
+
+/**
+ * Setting port to default port if one is set, if not (local), set port to 3000 and start
+ * server.
+ */
+let port = process.env.port || 3000;
+app.listen(port, function () {
+	console.log("Listening on port " + port);
+});
+
+module.exports = app;
